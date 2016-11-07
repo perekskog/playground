fe18bd5b4c6b9e9bc91148269bc47c33282dc5c6
Per Ekskog <per.ekskog@gmail.com>
2016-11-07 17:07:35 +0100

    CsvJsonReport: Converted singlecolumn.csv

--- CsvJsonReport/singlecolumn.py
+++ CsvJsonReport/singlecolumn.py
@@ -6,14 +6,35 @@ import json
 def fetch_items(file):
     items = list()
     session = str()
+    task = str()
+    starttime = str()
+    stoptime = str()
     with open(file, 'r') as f:
         reader = csv.reader(f, delimiter='\t')
         for row in reader:
             if(len(row)==1):
+                # Start of session
                 session = row[0]
             else:
-                item = {'session': session, 'task':row[0], 'time': row[1]}
-                items.append(item)
+                # Not start of session
+                if row[0] == "":
+                    # Stop and add ongoing task, don't start new
+                    stoptime = row[1]
+                    item = {'session': session, 'task':task, 'start': starttime, 'stop': stoptime}
+                    items.append(item)
+                    task = ""
+                else:
+                    if task == "":
+                        # No ongoing task, start a new task
+                        task = row[0]
+                        starttime = row[1]
+                    else:
+                        # Stop and add ongoing task, start new task
+                        stoptime = row[1]
+                        item = {'session': session, 'task':task, 'start': starttime, 'stop': stoptime}
+                        items.append(item)
+                        task = row[0]
+                        starttime = row[1]
     return items
 
 
