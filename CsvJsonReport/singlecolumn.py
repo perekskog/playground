1a3fe41d46dd840bb3f9979dadcc6b3846c79759
Per Ekskog <per.ekskog@ericsson.com>
2016-11-09 13:59:44 +0100

    CsvJsonReport: Successfully parsed multicolumn.csv.

--- CsvJsonReport/singlecolumn.py
+++ CsvJsonReport/singlecolumn.py
@@ -3,10 +3,11 @@ import sys
 import json
 
 
-def fetch_items(file):
+def fetch_items(file, skipcolumns):
     sessions = list()
     taskentries = list()
     sessionname = str()
+    sessioncreated = str()
     sessionisongoing = False
     taskname = str()
     starttime = str()
@@ -14,48 +15,65 @@ def fetch_items(file):
     with open(file, 'r') as f:
         reader = csv.reader(f, delimiter='\t')
         for row in reader:
-            if(len(row)==1):
+            print(row)
+            if(len(row)<=skipcolumns):
+                print("continue 1")
+                continue;
+            if(len(row)>=skipcolumns+1 and row[0+skipcolumns]=="" and row[1+skipcolumns]==""):
+                print("continue 2")
+                continue;
+            if(len(row)==1+skipcolumns or (len(row)>=2+skipcolumns and row[1+skipcolumns] == "")):
                 if sessionname == "":
                     # Start of new sesseion
-                    sessionname = row[0]
-                    if sessionname.startswith("* "):
+                    sessioncreated = row[0+skipcolumns].split()[-1]
+                    if row[0+skipcolumns].startswith("*"):
+                        print("case s1")
                         sessionisongoing = True
-                        sessionname = sessionname.lstrip("* ")
+                        sessionname = row[0+skipcolumns].split()[1]
                     else:
+                        print("case s2")
                         sessionisongoing = False
+                        sessionname = row[0+skipcolumns].split()[0]
                 else:
                     # Switch session
-                    session = {'sessionname': sessionname, 'isongoing': sessionisongoing, 'taskentries': taskentries}
+                    session = {'sessionname': sessionname, 'isongoing': sessionisongoing, 'created': sessioncreated, 'taskentries': taskentries}
                     sessions.append(session)
                     taskentries = list()
-                    sessionname = row[0]
-                    if sessionname.startswith("* "):
+                    sessioncreated = row[0+skipcolumns].split()[-1]
+                    if row[0+skipcolumns].startswith("*"):
+                        print("case s3")
                         sessionisongoing = True
-                        sessionname = sessionname.lstrip("* ")
+                        sessionname = row[0+skipcolumns].split()[1]
                     else:
+                        print("case s4")
                         sessionisongoing = False
+                        sessionname = row[0+skipcolumns].split()[0]
             else:
                 # Not start of session
-                if row[0] == "":
+                print(len(row))
+                if row[0+skipcolumns] == "":
                     # Stop and add ongoing task, don't start new
-                    stoptime = row[1]
+                    print("case t1")
+                    stoptime = row[1+skipcolumns]
                     taskentry = {'taskname':taskname, 'start': starttime, 'stop': stoptime}
                     taskentries.append(taskentry)
                     taskname = ""
                 else:
                     if taskname == "":
                         # No ongoing task, start a new task
-                        taskname = row[0]
-                        starttime = row[1]
+                        print("case t2")
+                        taskname = row[0+skipcolumns]
+                        starttime = row[1+skipcolumns]
                     else:
                         # Stop and add ongoing task, start new task
-                        stoptime = row[1]
+                        print("case t3")
+                        stoptime = row[1+skipcolumns]
                         taskentry = {'taskname':taskname, 'start': starttime, 'stop': stoptime}
                         taskentries.append(taskentry)
-                        taskname = row[0]
-                        starttime = row[1]
+                        taskname = row[0+skipcolumns]
+                        starttime = row[1+skipcolumns]
     if len(taskentries) > 0:
-        session = {'sessionname': sessionname, 'isongoing': sessionisongoing, 'taskentries': taskentries}
+        session = {'sessionname': sessionname, 'isongoing': sessionisongoing, 'created': sessioncreated, 'taskentries': taskentries}
         sessions.append(session)
 
     return sessions
@@ -65,12 +83,12 @@ def print_items(items):
     print(items)
 
 
-def main(file):
-    items = fetch_items(file)
+def main(file, skipcolumns):
+    items = fetch_items(file, int(skipcolumns))
 #    print_items(items)
     jsonitems = json.dumps(items, sort_keys=True, indent=4)
     print_items(jsonitems)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
