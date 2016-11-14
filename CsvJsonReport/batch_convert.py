0422a309ee42c76c56de2e0d3cfc656473d18cd7
Per Ekskog <per.ekskog@gmail.com>
2016-11-14 23:20:10 +0100

    CsvJsonReport: Adding old sessions.

--- CsvJsonReport/batch_convert.py
+++ CsvJsonReport/batch_convert.py
@@ -4,21 +4,16 @@ from datetime import datetime
 from TpCsv2Json import fetch_items
 
 sheets = [
-    {
-    'name': "21 dec - 27 dec-Tabell 1.csv",
-    'basedate': "15-12-21",
-    'columns': [0, 2, 4, 6, 8, 10, 12]
-    },
-    {
-    'name': "30 dec - 4 jan-Tabell 1.csv",
-    'basedate': "15-12-30",
-    'columns': [0, 2, 4, 6, 8, 10]
-    },
-    {
-    'name': "5 jan - 10 jan-Tabell 1.csv",
-    'basedate': "16-01-05",
-    'columns': [0, 2, 4, 6, 8, 10]
-    }
+    { 'name': "21 dec - 27 dec-Tabell 1.csv", 'basedate': "15-12-21", 'columns': [0, 2, 4, 6, 8, 10, 12]},
+    { 'name': "30 dec - 4 jan-Tabell 1.csv", 'basedate': "15-12-30", 'columns': [0, 2, 4, 6, 8, 10]},
+    { 'name': "5 jan - 10 jan-Tabell 1.csv", 'basedate': "16-01-05", 'columns': [0, 2, 4, 6, 8, 10]},
+
+    { 'name': "11 jan - 17 jan-Tabell 1.csv", 'basedate': "16-01-11", 'columns': [1, 3, 5, 7, 9, 11, 13]},
+    { 'name': "18 jan - 22 jan-Tabell 1.csv", 'basedate': "16-01-18", 'columns': [0, 2, 4, 6, 8]},
+    { 'name': "25 jan - 31 jan-Tabell 1.csv", 'basedate': "16-01-25", 'columns': [0, 2, 4, 6, 8, 10, 12]},
+    { 'name': "1 feb - 7 feb-Tabell 1.csv", 'basedate': "16-02-01", 'columns': [0, 2, 4, 6, 8, 10, 12]},
+    { 'name': "8 feb - 14 feb-Tabell 1.csv", 'basedate': "16-02-08", 'columns': [0, 2, 4, 6, 8, 10, 12]},
+    { 'name': "15 feb - 21 feb-Tabell 1.csv", 'basedate': "16-02-15", 'columns': [0, 2, 4, 6, 8, 10, 12]},
 ]
 
 def date_handler(obj):
