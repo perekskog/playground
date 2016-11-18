f6aedef53294e75f26bab60703a87ab5f19b8467
Per Ekskog <per.ekskog@gmail.com>
2016-11-18 08:17:10 +0100

    wip

--- CsvJsonReport/kostnad.py
+++ CsvJsonReport/kostnad.py
@@ -21,24 +21,37 @@ def date_handler(obj):
         raise TypeError('Object of type %s with value of %s is not JSON serializable' % (type(obj), repr(obj)))
 
 
+def session_between_dates(session, projectname, startdate, enddate):
+    if session['projectname']==projectname and session['date_created'].date().toordinal() >= startdate and session['date_created'].date().toordinal() <= enddate:
+        return True
+    else:
+        return False
+
+
 def main(store, first_date, last_date):
 #    items = fetch_items(file)
     items = json.load(open(store, 'r', encoding="utf-8"), object_hook=datetime_parser)
-
-    # Filter
-    kostnad = [ s for s in items if s['projectname']=="Kostnad"]
+    # Filter: Select subset of sessions
+    # Merge filters, use a function for filtering
     first = datetime.strptime(first_date, "%y-%m-%d").date().toordinal()
     last = datetime.strptime(last_date, "%y-%m-%d").date().toordinal()
-    subset_kostnad = [ s for s in kostnad if s['date_created'].date().toordinal() >= first and
-                                        s['date_created'].date().toordinal() <= last ]
+    subset_kostnad = [ s for s in items if session_between_dates(s, "Kostnad", first, last)]
 
-    # Map
-    # ...
+    # For each session:
+    # Map to [date, taskname, totaltime]
+    for session in subset_kostnad:
+        task_summary = { (t['taskname'], timedelta(0)) for t in session['taskentries'] }
+        for taskentry in session['taskentries']:
+            name = taskentry['taskname']
+            totaltime = taskentry['stop']-taskentry['start']
+            task_summary[taskname] += totaltime
+        print(task_summary)
 
+    # Print result
+    print("subset_kostnad")
     for session in subset_kostnad:
         print(session['projectname'], session['date_created'])
 
-datetime.strptime("16-11-10", "%y-%m-%d")
 
 
 if __name__ == '__main__':
