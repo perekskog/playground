933fb48fc5edc9edb193e7ca4b947d72479cdc03
Per Ekskog <per.ekskog@gmail.com>
2016-12-02 17:32:18 +0100

    IngestFilmer: ingest skeleton

--- IngestFilmer/ingestMovies.py
+++ IngestFilmer/ingestMovies.py
@@ -1,8 +1,18 @@
 import sys
+import json
+
+
+def fetch_items(filein):
+    movies = list()
+    with open(filein, 'r', encoding='utf-8') as f:
+        for row in f:
+            print(row)
+    return movies
 
 
 def main(filein, fileout):
-    return
+    items = fetch_items(filein)
+    open(fileout, 'wb').write(json.dumps(items, sort_keys=True, indent=4, ensure_ascii=False).encode('utf8'))
 
 
 if __name__ == "__main__":
