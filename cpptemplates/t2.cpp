a28ab6eb8ca9ffbbd3ecd0dce2a0a7f1588a4baa
Per Ekskog <per.ekskog@gmail.com>
2016-12-07 20:59:56 +0100

    cpptemplates: wip

--- cpptemplates/t2.cpp
+++ cpptemplates/t2.cpp
@@ -3,22 +3,15 @@
 template<class T>
 const T& pe_max(const T& a, const T& b)
 {
-    if(a>=b)
-        return a;
-    else
-        return b;
+    return a>=b ? a : b;
 };
-char pe_max(char a, int b)
-{
-    return pe_max(a, static_cast<char>(b));
-}
-
+char pe_max(char a, int b) { return pe_max<char>(a, b); }
 
 int main(int argc, char*argv[])
 {
     using namespace std;
 
     cout << pe_max(1,2) << endl;
-    cout << pe_max('a', 2) << endl;
+    cout << pe_max<char>('a', 2) << endl;
     cout << pe_max('b', 'a') << endl;
 }
