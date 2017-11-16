24a6a1064311da9682d3460e883e400387678026
Per Ekskog <per.ekskog@gmail.com>
2017-11-16 17:08:17 +0100

    Cucumber: Moved Calculator

--- Cucumber/src/test/java/se/perekskog/common/stepDefinition.java
+++ Cucumber/src/test/java/se/perekskog/common/stepDefinition.java
@@ -3,12 +3,18 @@ package se.perekskog.common;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.PendingException;
+import se.perekskog.Calculator;
 
 public class stepDefinition {
+
+    private Calculator calculator;
 
 @Given("^Connect to Test server$")
 public void connect_to_Test_server() throws Throwable {
     System.out.println("===C===connect_to_Test_server");
+
+    calculator = new Calculator();
+    calculator.add(5);
     // Write code here that turns the phrase above into concrete actions
     //throw new PendingException();
 }
@@ -16,6 +22,8 @@ public void connect_to_Test_server() throws Throwable {
 @Then("^cleanup locally created files$")
 public void cleanup_locally_created_files() throws Throwable {
     System.out.println("===C===cleanup_locally_created_files");
+    calculator.add(3);
+    System.out.println("===C===" + calculator.state());
     // Write code here that turns the phrase above into concrete actions
     //throw new PendingException();
 }
